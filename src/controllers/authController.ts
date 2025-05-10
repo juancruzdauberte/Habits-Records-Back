09815cd76c990d4bc420c4903047b7df3e8e0584
Juan Cruz Dauberte <juandauberte@gmail.com>
2025-05-10 11:43:50 -0300

    add: status and logout endpoints

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,9 @@
 import passport from "passport";
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import { AuthenticatedRequest } from "../types/types";
 import { generateJwt } from "../utils/generateJwt";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
 class AuthController {
   static googleAuth = passport.authenticate("google", {
     scope: ["profile", "email"],
@@ -10,15 +12,45 @@ class AuthController {
   static redirectAfterLogin = (req: Request, res: Response) => {
     const { id, email } = (req as AuthenticatedRequest).user!;
     const token = generateJwt({ id, email });
-    res.status(200).json({
-      message: "Autenticación exitosa",
-      token,
+    // res.status(200).json({
+    //   message: "Autenticación exitosa",
+    //   token,
+    // });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
     });
 
-    // res.redirect(`${config.CLIENT_URL}/home`);
+    res.redirect(`${config.CLIENT_URL}/home`);
   };
 
-  static logOut = () => {};
+  static logOut = (req: Request, res: Response) => {
+    req.logout((err) => {
+      if (err) {
+        return res.status(500).json({ message: "Error al cerrar sesión" });
+      }
+
+      res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+      });
+
+      res.redirect(`${config.CLIENT_URL}`);
+    });
+  };
+
+  static userStatus = (req: Request, res: Response) => {
+    if (req.user) {
+      res.json({ login: true, user: req.user });
+      return;
+    } else {
+      res.status(401).json({ login: false });
+      return;
+    }
+  };
 }
 
 export default AuthController;
--- src/routers/auth.routes.ts
+++ src/routers/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthController from "../controllers/authController.ts";
 import { logInMiddleware } from "../middlewares/auth/logInMiddleware.ts";
+import { isAuthenticated } from "../middlewares/auth/authenticatedMiddleware.ts";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.get(
   logInMiddleware,
   AuthController.redirectAfterLogin
 );
+router.get("/status", isAuthenticated, AuthController.userStatus);
+router.get("/logout", isAuthenticated, AuthController.logOut);
 
 export default router;
